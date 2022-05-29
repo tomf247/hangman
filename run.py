f3e4eeead6288f4c6123e3591cb990f871f87bbd
tomf247 <tomfoley247@gmail.com>
2022-05-29 14:44:45 +0000

    Import random library and create function to select a random word from list

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+def random_word():
+    """
+    Selects a random word for the game
+    """
+
+    words = [
+        'finch',
+        'pigeon',
+        'pheasant',
+        'duck',
+        'owl',
+        'crow',
+        'sparrow',
+        'robin'
+        ]
+    return words[random.randint(0, len(words)-1)]
+
+
+print(random_word())
+
